c5b6e8409b8d8df06806a32df4a39ca462242359
thanhnt <tatthanh.dev@gmail.com>
2017-05-05 16:42:19 +0700

    store data into condominiumsmanilas table

--- app/Http/Controllers/Admin/CrawlerController.php
+++ app/Http/Controllers/Admin/CrawlerController.php
@@ -7,9 +7,20 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\BaseRequest;
 use Maatwebsite\Excel\Excel;
 use Yangqi\Htmldom\Htmldom;
+use App\Repositories\CondominiumsmanilaRepositoryInterface;
 
 class CrawlerController extends Controller
 {
+    /** @var \App\Repositories\CondominiumsmanilaRepositoryInterface */
+    protected $condominiumsmanilaRepository;
+
+    public function __construct(
+        CondominiumsmanilaRepositoryInterface      $condominiumsmanilaRepository
+    )
+    {
+        $this->condominiumsmanilaRepository        = $condominiumsmanilaRepository;
+    }
+
     public function index()
     {
         return view('pages.admin.' . config('view.admin') . '.crawlers.index', [
@@ -33,17 +44,45 @@ class CrawlerController extends Controller
         $condosFields = array_fill_keys($condosFields, "");
 
         foreach ($condos as $key => $condo) {
-            $condos[$key] = array_merge($condosFields, $condo);
+//            $condos[$key] = array_merge($condosFields, $condo);
+            $this->condominiumsmanilaRepository->create(
+                [
+                    'title'           => isset($condo['title']) ? $condo['title'] : 'null',
+                    'postal_code'     => null,
+                    'country'         => 'philippine',
+                    'province'        => null,
+                    'city'            => null,
+                    'address'         => isset($condo['condo_location']) ? $condo['condo_location'] : null,
+                    'building_type'   => isset($condo['type']) ? $condo['type'] : null,
+                    'latitude'        => 0,
+                    'longitude'       => 0,
+                    'completion_year' => null,
+                    'number_floor'    => isset($condo['condo_levels']) ? intval($condo['condo_levels']) : null,
+                    'number_unit'     => null,
+                    'developer_name'  => isset($condo['developer']) ? $condo['developer'] : null,
+                    'facilities'      => isset($condo['facilitiesand_services']) ? $condo['facilitiesand_services'] : null,
+                    'unit_size'       => isset($condo['available_unit_sizes']) ? $condo['available_unit_sizes'] : null,
+                    'condo_url'       => null,
+                    'developer_url'   => null,
+                ]
+            );
         }
 
-        \Excel::create('condominiumsmanila.com', function($excel) use ($condosFields, $condos) {
+        return redirect()
+            ->action('Admin\CrawlerController@index')
+            ->with(
+                'message-success',
+                trans('admin.messages.general.create_success')
+            );
 
-            $excel->sheet('condominiumsmanila.com', function($sheet) use ($condosFields, $condos) {
-                $sheet->row(1, array_keys($condosFields));
-                $sheet->fromArray($condos);
-            });
-
-        })->export('csv');
+//        \Excel::create('condominiumsmanila.com', function($excel) use ($condosFields, $condos) {
+//
+//            $excel->sheet('condominiumsmanila.com', function($sheet) use ($condosFields, $condos) {
+//                $sheet->row(1, array_keys($condosFields));
+//                $sheet->fromArray($condos);
+//            });
+//
+//        })->export('csv');
     }
 
     public function getListCondominiumsmanila($url)
