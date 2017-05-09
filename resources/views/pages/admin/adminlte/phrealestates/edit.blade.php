eec9839805e37923f5e6dc9be281ae700919a93b
thanhnt <tatthanh.dev@gmail.com>
2017-05-09 11:41:21 +0700

    disabled required

--- resources/views/pages/admin/adminlte/phrealestates/edit.blade.php
+++ resources/views/pages/admin/adminlte/phrealestates/edit.blade.php
@@ -63,7 +63,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('title')) has-error @endif">
                             <label for="title">@lang('admin.pages.phrealestates.columns.title')</label>
-                            <input type="text" class="form-control" id="title" name="title" required
+                            <input type="text" class="form-control" id="title" name="title"
                                    value="{{ old('title') ? old('title') : $phrealestate->title }}">
                         </div>
                     </div>
@@ -73,7 +73,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('postal_code')) has-error @endif">
                             <label for="postal_code">@lang('admin.pages.phrealestates.columns.postal_code')</label>
-                            <input type="text" class="form-control" id="postal_code" name="postal_code" required
+                            <input type="text" class="form-control" id="postal_code" name="postal_code"
                                    value="{{ old('postal_code') ? old('postal_code') : $phrealestate->postal_code }}">
                         </div>
                     </div>
@@ -83,7 +83,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('country')) has-error @endif">
                             <label for="country">@lang('admin.pages.phrealestates.columns.country')</label>
-                            <input type="text" class="form-control" id="country" name="country" required
+                            <input type="text" class="form-control" id="country" name="country"
                                    value="{{ old('country') ? old('country') : $phrealestate->country }}">
                         </div>
                     </div>
@@ -93,7 +93,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('province')) has-error @endif">
                             <label for="province">@lang('admin.pages.phrealestates.columns.province')</label>
-                            <input type="text" class="form-control" id="province" name="province" required
+                            <input type="text" class="form-control" id="province" name="province"
                                    value="{{ old('province') ? old('province') : $phrealestate->province }}">
                         </div>
                     </div>
@@ -103,7 +103,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('city')) has-error @endif">
                             <label for="city">@lang('admin.pages.phrealestates.columns.city')</label>
-                            <input type="text" class="form-control" id="city" name="city" required
+                            <input type="text" class="form-control" id="city" name="city"
                                    value="{{ old('city') ? old('city') : $phrealestate->city }}">
                         </div>
                     </div>
@@ -113,7 +113,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('address')) has-error @endif">
                             <label for="address">@lang('admin.pages.phrealestates.columns.address')</label>
-                            <input type="text" class="form-control" id="address" name="address" required
+                            <input type="text" class="form-control" id="address" name="address"
                                    value="{{ old('address') ? old('address') : $phrealestate->address }}">
                         </div>
                     </div>
@@ -123,7 +123,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('building_type')) has-error @endif">
                             <label for="building_type">@lang('admin.pages.phrealestates.columns.building_type')</label>
-                            <input type="text" class="form-control" id="building_type" name="building_type" required
+                            <input type="text" class="form-control" id="building_type" name="building_type"
                                    value="{{ old('building_type') ? old('building_type') : $phrealestate->building_type }}">
                         </div>
                     </div>
@@ -133,7 +133,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('latitude')) has-error @endif">
                             <label for="latitude">@lang('admin.pages.phrealestates.columns.latitude')</label>
-                            <input type="text" class="form-control" id="latitude" name="latitude" required
+                            <input type="text" class="form-control" id="latitude" name="latitude"
                                    value="{{ old('latitude') ? old('latitude') : $phrealestate->latitude }}">
                         </div>
                     </div>
@@ -143,7 +143,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('longitude')) has-error @endif">
                             <label for="longitude">@lang('admin.pages.phrealestates.columns.longitude')</label>
-                            <input type="text" class="form-control" id="longitude" name="longitude" required
+                            <input type="text" class="form-control" id="longitude" name="longitude"
                                    value="{{ old('longitude') ? old('longitude') : $phrealestate->longitude }}">
                         </div>
                     </div>
@@ -153,7 +153,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('completion_year')) has-error @endif">
                             <label for="completion_year">@lang('admin.pages.phrealestates.columns.completion_year')</label>
-                            <input type="text" class="form-control" id="completion_year" name="completion_year" required
+                            <input type="text" class="form-control" id="completion_year" name="completion_year"
                                    value="{{ old('completion_year') ? old('completion_year') : $phrealestate->completion_year }}">
                         </div>
                     </div>
@@ -163,7 +163,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('number_floor')) has-error @endif">
                             <label for="number_floor">@lang('admin.pages.phrealestates.columns.number_floor')</label>
-                            <input type="text" class="form-control" id="number_floor" name="number_floor" required
+                            <input type="text" class="form-control" id="number_floor" name="number_floor"
                                    value="{{ old('number_floor') ? old('number_floor') : $phrealestate->number_floor }}">
                         </div>
                     </div>
@@ -173,7 +173,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('number_unit')) has-error @endif">
                             <label for="number_unit">@lang('admin.pages.phrealestates.columns.number_unit')</label>
-                            <input type="text" class="form-control" id="number_unit" name="number_unit" required
+                            <input type="text" class="form-control" id="number_unit" name="number_unit"
                                    value="{{ old('number_unit') ? old('number_unit') : $phrealestate->number_unit }}">
                         </div>
                     </div>
@@ -183,7 +183,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('developer_name')) has-error @endif">
                             <label for="developer_name">@lang('admin.pages.phrealestates.columns.developer_name')</label>
-                            <input type="text" class="form-control" id="developer_name" name="developer_name" required
+                            <input type="text" class="form-control" id="developer_name" name="developer_name"
                                    value="{{ old('developer_name') ? old('developer_name') : $phrealestate->developer_name }}">
                         </div>
                     </div>
@@ -193,7 +193,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('facilities')) has-error @endif">
                             <label for="facilities">@lang('admin.pages.phrealestates.columns.facilities')</label>
-                            <textarea name="facilities" class="form-control" rows="5" required
+                            <textarea name="facilities" class="form-control" rows="5"
                                       placeholder="@lang('admin.pages.phrealestates.columns.facilities')">{{ old('facilities') ? old('facilities') : $phrealestate->facilities }}</textarea>
                         </div>
                     </div>
@@ -203,7 +203,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('unit_size')) has-error @endif">
                             <label for="unit_size">@lang('admin.pages.phrealestates.columns.unit_size')</label>
-                            <input type="text" class="form-control" id="unit_size" name="unit_size" required
+                            <input type="text" class="form-control" id="unit_size" name="unit_size"
                                    value="{{ old('unit_size') ? old('unit_size') : $phrealestate->unit_size }}">
                         </div>
                     </div>
@@ -213,7 +213,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('condo_url')) has-error @endif">
                             <label for="condo_url">@lang('admin.pages.phrealestates.columns.condo_url')</label>
-                            <input type="text" class="form-control" id="condo_url" name="condo_url" required
+                            <input type="text" class="form-control" id="condo_url" name="condo_url"
                                    value="{{ old('condo_url') ? old('condo_url') : $phrealestate->condo_url }}">
                         </div>
                     </div>
@@ -223,7 +223,7 @@
                     <div class="col-md-12">
                         <div class="form-group @if ($errors->has('developer_url')) has-error @endif">
                             <label for="developer_url">@lang('admin.pages.phrealestates.columns.developer_url')</label>
-                            <input type="text" class="form-control" id="developer_url" name="developer_url" required
+                            <input type="text" class="form-control" id="developer_url" name="developer_url" 
                                    value="{{ old('developer_url') ? old('developer_url') : $phrealestate->developer_url }}">
                         </div>
                     </div>
