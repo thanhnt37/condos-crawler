57329a255e00c1bd0b29a17053ee50270d370cf1
thanhnt <tatthanh.dev@gmail.com>
2017-05-11 19:36:51 +0700

    re-arrange left navigation

--- resources/views/layouts/admin/adminlte/left_navigation.blade.php
+++ resources/views/layouts/admin/adminlte/left_navigation.blade.php
@@ -24,7 +24,7 @@
         <!-- /.search form -->
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li>
+            <li class="header">Main Actions</li>
 
             <li @if( $menu=='dashboard') class="active" @endif ><a href="{!! \URL::action('Admin\IndexController@index') !!}"><i class="fa fa-dashboard"></i> <span>@lang('admin.menu.dashboard')</span></a></li>
 
@@ -40,13 +40,15 @@
                 {{--<li @if( $menu=='logs') class="active" @endif ><a href="{!! \URL::action('Admin\LogController@index') !!}"><i class="fa fa-sticky-note-o"></i> <span>@lang('admin.menu.log_system')</span></a></li>--}}
                 {{--<li @if( $menu=='images') class="active" @endif ><a href="{!! \URL::action('Admin\ImageController@index') !!}"><i class="fa fa-file-image-o"></i> <span>@lang('admin.menu.images')</span></a></li>--}}
                 {{--<li @if( $menu=='articles') class="active" @endif ><a href="{!! \URL::action('Admin\ArticleController@index') !!}"><i class="fa fa-file-word-o"></i> <span>@lang('admin.menu.articles')</span></a></li>--}}
-                <li @if( $menu=='buildings') class="active" @endif ><a href="{!! \URL::action('Admin\BuildingController@index') !!}"><i class="fa fa-file-word-o"></i> <span>Compare Data</span></a></li>
                 <li @if( $menu=='crawlers') class="active" @endif ><a href="{!! \URL::action('Admin\CrawlerController@index') !!}"><i class="fa fa-file-word-o"></i> <span>@lang('admin.menu.crawlers')</span></a></li>
+                <li @if( $menu=='buildings') class="active" @endif ><a href="{!! \URL::action('Admin\BuildingController@index') !!}"><i class="fa fa-file-word-o"></i> <span>Compare Data</span></a></li>
+                <li @if( $menu=='propertyasias') class="active" @endif ><a href="{!! \URL::action('Admin\PropertyasiaController@index') !!}"><i class="fa fa-users"></i> <span>Collections</span></a></li>
             @endif
+
+            <li class="header">Condos Crawled</li>
             <li @if( $menu=='condominiumsmanilas') class="active" @endif ><a href="{!! \URL::action('Admin\CondominiumsmanilaController@index') !!}"><i class="fa fa-users"></i> <span>Condominiumsmanilas</span></a></li>
             <li @if( $menu=='phrealestates') class="active" @endif ><a href="{!! \URL::action('Admin\PhrealestateController@index') !!}"><i class="fa fa-users"></i> <span>Phrealestates</span></a></li>
             <li @if( $menu=='philpropertyexperts') class="active" @endif ><a href="{!! \URL::action('Admin\PhilpropertyexpertController@index') !!}"><i class="fa fa-users"></i> <span>Philpropertyexperts</span></a></li>
-            <li @if( $menu=='propertyasias') class="active" @endif ><a href="{!! \URL::action('Admin\PropertyasiaController@index') !!}"><i class="fa fa-users"></i> <span>Propertyasia</span></a></li>
             <!-- %%SIDEMENU%% -->
         </ul>
     </section>
